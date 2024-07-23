b811c07569ed05b85521f99d698b5c2714b7e1e2
Daniella Matute <daniella.l.matute@gmail.com>
2024-07-23 20:54:59 +0000

    The command 'let' is a bash builtin, so if you're using it in a script that runs /bin/sh, it will fail, since /bin/sh on Ubuntu is dash, not bash.

--- phage_finder_v2.5_4docker/bin/HMM3_searches.sh
+++ phage_finder_v2.5_4docker/bin/HMM3_searches.sh
@@ -4,7 +4,7 @@
 Bin="$(cd -- "$(dirname "$0")" && pwd)"
 phome=${Bin%/*}
 pepfile=$1
-src="/scratch/dfouts/bin"
+
 base=`pwd`
 
 if [ ! -d HMM3_searches_dir ] # check if directory is present
@@ -18,11 +18,11 @@ cd ${base}/HMM3_searches_dir/
 total=`cat ${phome}/hmm3.lst | wc -l | sed 's/^ *//'`
 for i in `cat ${phome}/hmm3.lst`
 do
-  let "count = count + 1"
+  count=$((count + 1))
   Result=`echo $count $total | awk '{printf( "%3.1f\n", (($1/$2) * 100))}'`
   echo -ne "\r    $Result% complete"
   #echo -ne "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b$Result% complete"
-  ${src}/hmmsearch ${phome}/PHAGE_HMM3s_dir/${i}.HMM ${base}/${pepfile} >> $i.out
+  hmmsearch ${phome}/PHAGE_HMM3s_dir/${i}.HMM ${base}/${pepfile} >> $i.out
 done
 echo
 cat *.out >> ../combined.hmm3
